4de73c78b29160b60750c095ba1ec48cb1ea6609
JamesMutahi <mutahijames0@gmail.com>
2019-07-29 09:38:50 +0300

    Make minor changes

--- instagram/forms.py
+++ instagram/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Post
+from .models import *
+
 
 # ......
 class NewPostForm(forms.ModelForm):
@@ -9,3 +10,19 @@ class NewPostForm(forms.ModelForm):
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
         }
+
+
+class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget = forms.TextInput()
+
+    class Meta:
+        model = Profile
+        fields = ('name', 'image', 'bio')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user',)
